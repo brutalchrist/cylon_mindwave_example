45c279d99e52a997059b2d08e9ee03bc1e74ef50
brutalchrist <brutalchrist@gmail.com>
2017-03-13 12:55:34 -0300

    First commit

---
+++ Robot.js
@@ -0,0 +1,102 @@
+var PythonShell = require('python-shell');
+var Cylon = require('cylon');
+
+var ATTENTION_NUMBERS = 3;
+
+var mapping_devices = 
+  {
+    666: {
+      name: 'relay_1',
+      action: function(device) {
+        device.toggle();
+      }
+    },
+    755: {
+      name: 'relay_2',
+      action: function(device) {
+        device.toggle();
+      }
+    }
+  }
+;
+
+Cylon.robot({
+  connections: {
+    neurosky: {
+      adaptor: "neurosky",
+      port: '/dev/cu.MindWaveMobile-SerialPo' 
+    },
+    arduino: {
+      adaptor: 'firmata',
+      port: '/dev/tty.usbmodem1411' 
+    }
+  },
+
+  devices: {
+    headset: { driver: "neurosky", connection: 'neurosky'},
+    relay_1: {
+      driver: 'relay',
+      pin: 6,
+      type: 'open',
+      connection: 'arduino'
+    },
+    relay_2: {
+      driver: 'relay',
+      pin: 7,
+      type: 'open',
+      connection: 'arduino'
+    }
+  },
+
+  work: function(my) {
+    var attention_data = 0;
+    var meditation_data = 0;
+    var attention_counter = 0;
+
+    var device_return = null;
+    var pyshell = new PythonShell('python/markerdetect.py');
+
+    every((1).second(), function() {
+      if (attention_data > 80) {
+        attention_counter++;
+      }
+
+      // if (meditation_data > 10) {
+      //   attention_counter = 0;
+      // }
+
+      console.log('attention_data: ' + attention_data);
+      // console.log('meditation_data: ' + meditation_data);
+      console.log('attention_counter: ' + attention_counter);
+      console.log('===================');
+
+      if (attention_counter >= ATTENTION_NUMBERS) {
+        if (device_return != null) {
+          console.log(device_return);
+          var device = mapping_devices[device_return];
+          device.action(my[device.name])
+          attention_counter = 0;
+        }
+      }
+    });
+
+    my.headset.on("attention", function(data) {
+      attention_data = data;
+    });
+
+    // my.headset.on("meditation", function(data) {
+    //   meditation_data = data;
+    // });
+    
+
+    pyshell.on('message', function (message) {
+      console.log(message);
+      device_return = message;
+    });
+
+    pyshell.end(function (err) {
+      if (err) throw err;
+      console.log('finished');
+    });
+  }
+}).start();
---
+++ python/markerdetect.py
@@ -0,0 +1,32 @@
+#!/usr/local/opt/python/bin/python2.7
+
+import cv2
+import sys
+
+from ar_markers.hamming.detect import detect_markers
+
+if __name__ == '__main__':
+
+    # for line in sys.stdin:
+    capture = cv2.VideoCapture(1)
+
+    if capture.isOpened():
+        frame_captured, frame = capture.read()
+    else:
+        frame_captured = False
+    while frame_captured:
+        markers = detect_markers(frame)
+        for marker in markers:
+            marker.highlite_marker(frame)
+            print marker.id
+            sys.stdout.flush()
+            #exit()
+        # cv2.imshow('Test Frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        frame_captured, frame = capture.read()
+
+    # When everything done, release the capture
+    capture.release()
+    cv2.destroyAllWindows()
+
